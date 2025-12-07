cb322f186cc9b89c165307636519be7efbbda17a
Sam <samuel.wickstead@gmail.com>
2025-12-07 17:32:40 +0000

    Updated broadcast message

--- controllers/drone/drone.py
+++ controllers/drone/drone.py
@@ -72,8 +72,6 @@ time_between_broadcasts = 1000
 
 ## Main ##
 time_since_last_broadcast = 0
-x = 0
-y = 0
 
 #Simulate one timetep to initialise sensor readings
 robot.step(timestep)
@@ -83,12 +81,11 @@ od = Odometry(0, 0, 0, left_sensor.getValue(), right_sensor.getValue())
 
 while robot.step(timestep) != -1:
     od.step(left_sensor.getValue(), right_sensor.getValue())
-    print(f"X: {od.x}, Y: {od.y}")
     time_since_last_broadcast += timestep
     if time_since_last_broadcast > time_between_broadcasts:
         message = { "name":name,
-                    "x": x,
-                    "y": y}
+                    "x": od.x,
+                    "y": od.y}
         emitter.send(str(message))
         time_since_last_broadcast = 0
     
